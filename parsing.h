279dbbf85de319562b509cf088ef2b60bb0ab4d0
Daegue Lee <daegulee@c1r7s3.42seoul.kr>
2022-09-20 21:58:28 +0900

    grammer proto_modify

--- parsing.h
+++ parsing.h
@@ -6,7 +6,7 @@
 /*   By: daegulee <daegulee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 16:05:53 by idaegyu           #+#    #+#             */
-/*   Updated: 2022/09/20 20:41:59 by daegulee         ###   ########.fr       */
+/*   Updated: 2022/09/20 21:58:14 by daegulee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,18 +38,18 @@
 // command_elem -> redir 
 // command_elem -> 'word'
 
-// redir -> 'redir_left(<)' word
-// redir -> redir_right(>) word
-// redir -> redir_d_left(<<) word
-// redir -> redir_d_right(>>) word
-
+// redir -> 'redir_left(<)' redir_word
+// redir -> redir_right(>) redir_word
+// redir -> redir_d_left(<<) redir_word
+// redir -> redir_d_right(>>) redir_word
+// redir_word -> 'word'
 // pipe -> 'PIPE' simple_command
 // pipe -> pipe 'PIPE' simple_command 
 
 // cmd 1 
 // cmd1 | cmd2 
 // Data_list -> simple_command
-// Data_list -> simple_command pipe		
+// Data_list -> Data_list pipe		
 
 
 												//Data_list
@@ -58,14 +58,26 @@
 // 							simple_command 						PIPE										
 //      simple_command  command_elem			command elem 					simple_command						
 //state (No comd)  		(cmd)		
-//											redir					simple_command	command_elem		PIPE				simple_command
-//				
-//				simple_coommand	
-//		command_elem	command_elem
-//state	 (NO comd)		(cmd)																							command_elem 	command_elem	command_elem	
-//       word 			word			redir_left word			   pipe	word	word	       pipe   	word   			word 				word
-//       cat      filename      			  <a                   |    grep    123                |         ls  		 -al 				 $h
+//											redir					simple_command	command_elem		PIPE				simple_command	
+																		
 
+
+																									//DATA_list
+																		//DATA_list
+															//Data_list
+								//simple_command						
+//				simple_command												pipe										pipe
+//		command_elem	command_elem		command_elem						simple_command								simple_command
+//state	 (NO comd)		(cmd)				redir						command_elem	command_elem 				Command_elem	command_elem									command_elem 	command_elem	command_elem	
+//       word 			word			redir_left word			   PIPE	word			word	       		PIPE   	word   			word 			
+//       cat      filename      			  <a                   |    grep   			 123                |         ls  		 -al 				 
+
+//		
+
+																		//AST
+								//cmd													
+								//			redir_data						cmd									cmd
+//			cat		filename    									grep		123							ls		-al
 
 // 			main
 //	cat			grep
